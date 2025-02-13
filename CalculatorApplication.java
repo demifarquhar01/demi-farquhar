6dbc4f0e028f4955cd99a52eb14cc8317c2ff46f
demifarquhar01 <you@example.com>
2025-02-13 21:01:37 +0200

    Calculator application java file added

---
+++ CalculatorApplication.java
@@ -0,0 +1,6 @@
+/**
+ * This application is a simple calculator in java that performs addition, subtraction, multiplication, and division.
+ * The user has to select the which operation to perform 
+ * The application will run until the user select exit to stop the application
+ */
+
